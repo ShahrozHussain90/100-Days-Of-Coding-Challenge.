d0436c370e24b10429750fe4941ec0c4a9655fdd
ShahrozHussain90 <160394069+ShahrozHussain90@users.noreply.github.com>
2024-03-06 00:03:05 +0500

    Add files via upload

---
+++ Day03 Exercise/main.js
@@ -0,0 +1,11 @@
+//Number Eight: Write addition, subtraction, multiplication, 
+//and division operations that each result in the number 8. 
+//Be sure to enclose your operations in print statements to see the results.
+//Addition,
+console.log(6 + 2);
+//Subtraction,
+console.log(20 - 12);
+//Multiplication,
+console.log(4 * 2);
+//Division,
+console.log(16 / 2);
